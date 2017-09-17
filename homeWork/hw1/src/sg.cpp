7948b610be0eca4c72b7b6af8a6893891c8de077
FastStim <student2063@yandex.ru>
2017-09-18 00:37:53 +0300

    ok

---
+++ homeWork/hw1/src/sg.cpp
@@ -0,0 +1,36 @@
+using namespace std;
+
+class sg
+{
+public:
+	sg(int **);
+	~sg();
+
+private:
+	int** build(int**);
+	void findSolution(int**);
+
+};
+
+sg::sg (int **mat)
+{
+	cout << "test" << endl;
+
+	this->build(mat);
+	this->findSolution(mat);
+}
+
+sg::~sg()
+{
+
+}
+
+int** sg::build (int** mat)
+{
+	int** 
+}
+
+void sg::findSolution(int **mat)
+{
+
+}
