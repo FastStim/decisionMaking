e7a988a5c8a73a0e36168fdabbba22cf50988e91
FastStim <student2063@yandex.ru>
2017-12-04 15:18:07 +0300

    new fix

--- homeWork/hw4/src/main.cpp
+++ homeWork/hw4/src/main.cpp
@@ -9,8 +9,8 @@ int main()
 	int seb    = 15;
 	
 	int* count = new int[7]   {0, 50, 100, 150, 200, 250, 300};
-	float* p1  = new float[7] {0.2, 0.1, 0.2, 0.1, 0.1, 0.1, 0.05};
-	float* p2  = new float[7] {0.05, 0.1, 0.15, 0.1, 0.1, 0.1, 0.2};
+	float* p1  = new float[7] {0.2, 0.1, 0.2, 0.2, 0.15, 0.1, 0.05};
+	float* p2  = new float[7] {0.05, 0.1, 0.15, 0.2, 0.2, 0.1, 0.2};
 
 	bl* krit = new bl(sell_1, sell_2, sell_3, seb, count, p1, p2);
 	
