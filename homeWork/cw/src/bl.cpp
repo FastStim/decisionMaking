aadb6bbf25d247ff7f2fa2281a44d1cbf218df57
FastStim <student2063@yandex.ru>
2017-12-10 14:55:14 +0300

    edit

--- homeWork/cw/src/bl.cpp
+++ homeWork/cw/src/bl.cpp
@@ -11,37 +11,51 @@ struct flash
 class bl
 {
 public:
-	bl(string);
-	~bl();
+	bl(string, string, string, string, string, string);
 
-	void printTable();
+	void mPrintTable();
+	void bPrintTable(string);
+	void editBO(string);
 
 private:
 	vector<flash> allFlash;
+
 	vector<vector<int>> bType;
 	vector<vector<int>> bSize;
 	vector<vector<int>> bRSpeed;
 	vector<vector<int>> bWSpeed;
 	vector<vector<int>> bPrice;
 
-	vector<flash> parser(string);
-	void bl::printHeader(vector<int>);
-	void bl::printLine(vector<int>);
-	void bl::printSpace(int, int);
+	vector<flash> mParser(string);
+	vector<vector<int>> bParser(string);
+
+	string retCurRow(int, flash);
+
+	void mPrintHeader(vector<int>);
+	void bPrintHeader(string);
+	void mPrintLine(vector<int>);
+	void printSpace(int, int);
+
 };
 
-bl::bl (string model)
+bl::bl (string model, string mBType = "", string mBSize = "", string mBRSpeed = "", string mBWSpeed = "", string mBPrice = "")
 {
-	cout << "Конструктор получил: " << model << endl;
-	allFlash = parser(model);
+	allFlash = mParser(model);
+
+	if (mBType != "")
+		bType = bParser(mBType);
+	if (mBSize != "")
+		bSize = bParser(mBSize);
+	if (mBRSpeed != "")
+		bRSpeed = bParser(mBRSpeed);
+	if (mBWSpeed != "")
+		bWSpeed = bParser(mBWSpeed);
+	if (mBPrice != "")
+		bPrice = bParser(mBPrice);
 }
 
-bl::~bl()
-{
-
-}
-
-vector<flash> bl::parser(string csv)
+// Разбор основной таблицы
+vector<flash> bl::mParser(string csv)
 {
 	ifstream file(csv);
 	string str;
@@ -90,10 +104,45 @@ vector<flash> bl::parser(string csv)
 		curFlash.push_back(cur);
 	}
 
+	file.close();
 	return curFlash;
 }
 
-void bl::printTable()
+// Разбор биномиальных отношений
+vector<vector<int>> bl::bParser(string csv)
+{
+	ifstream file(csv);
+	string str;
+	
+	vector<vector<int>> curBio;
+
+	while(getline(file,str))
+	{
+		vector<int> cur;
+
+		string delim(";");
+		size_t prev = 0;
+		size_t next;
+		size_t delta = delim.length();
+
+		while((next = str.find(delim, prev)) != string::npos)
+		{
+			string tmp = str.substr(prev, next-prev);
+
+			cur.push_back(stoi(tmp));
+
+			prev = next + delta;
+		}
+
+		curBio.push_back(cur);
+	}
+
+	file.close();
+	return curBio;
+}
+
+// Печать таблицы с значениями
+void bl::mPrintTable()
 {
 	vector<int> cur;
 	for (int i = 0; i < 6; i++)
@@ -118,9 +167,9 @@ void bl::printTable()
 		i++;
 	}
 
-	printLine(cur);
-	printHeader(cur);
-	printLine(cur);
+	mPrintLine(cur);
+	mPrintHeader(cur);
+	mPrintLine(cur);
 
 	i = 0;
 	while (i < allFlash.size()) 
@@ -145,50 +194,260 @@ void bl::printTable()
 
 		cout << " |" << endl;
 
-		printLine(cur);
+		mPrintLine(cur);
 		i++;
 	}
 }
 
-void bl::printHeader(vector<int> cur)
+// Печать БО
+void bl::bPrintTable(string curBio)
+{
+	int cur = 0;
+	
+	int i = 0;
+	while (i < allFlash.size()) 
+	{
+		if(allFlash[i].name.length() > cur)
+			cur = allFlash[i].name.length();
+		i++;
+	}
+
+	vector<vector<int>> bio;
+	string title = "";
+
+	if (curBio == "bType")
+	{
+		bio = bType;
+		title = "Версия USB";
+	}
+	else if (curBio == "bSize")
+	{
+		bio = bSize;
+		title = "Размер памяти";
+	}
+	else if (curBio == "rSpeed")
+	{
+		bio = bRSpeed;
+		title = "Скорость чтения";
+	}
+	else if (curBio == "wSpeed")
+	{
+		bio = bWSpeed;
+		title = "Скорость записи";
+	}
+	else if (curBio == "price")
+	{
+		bio = bPrice;
+		title = "Цена";
+	}
+	else 
+		return;
+
+	bPrintHeader(title);
+
+	i = 0;
+	while (i < allFlash.size()) 
+	{
+		cout << allFlash[i].name;
+		printSpace(cur, allFlash[i].name.length());
+
+		int j = 0;
+		while (j < bio[i].size())
+		{
+			if (bio[i][j] == 1)
+				cout << "\033[32m";
+			else
+				cout << "\033[31m";
+			
+			cout << " " << bio[i][j] << "\033[0m";
+
+			j++;
+		}
+
+		cout << endl;
+
+		i++;
+	}
+}
+
+// Печать шапки таблицы
+void bl::mPrintHeader(vector<int> cur)
 {
 	string str;
 	str = "Name";
-	cout << "| " << str;
+	cout << "| " << "\033[33m" << str << "\033[0m";
 	printSpace(cur[0], str.length());
 
 	str = "Type";
-	cout << " | " << str;
+	cout << " | " << "\033[33m" << str << "\033[0m";
 	printSpace(cur[1], str.length());
 
 	str = "sz.";
-	cout << " | " << str;
+	cout << " | " << "\033[33m" << str << "\033[0m";
 	printSpace(cur[2], str.length());
 	
 	str = "rS.";
-	cout << " | " << str;
+	cout << " | " << "\033[33m" << str << "\033[0m";
 	printSpace(cur[3], str.length());
 	
 	str = "wS.";
-	cout << " | " << str;
+	cout << " | " << "\033[33m" << str << "\033[0m";
 	printSpace(cur[4], str.length());
 	
 	str = "prc.";
-	cout << " | " << str;
+	cout << " | " << "\033[33m" << str << "\033[0m";
 	printSpace(cur[5], str.length());
 
 	cout << " |" << endl;
 }
 
-void bl::printLine(vector<int> cur)
+// Печать заголовка таблицы
+void bl::bPrintHeader(string title)
+{
+	cout << "\033[33m" << title << ":" << "\033[0m" << endl;
+}
+
+// Печать линии между строками
+void bl::mPrintLine(vector<int> cur)
 {
 	for (int i = 0; i < (cur[0] + cur[1] + cur[2] + cur[3] + cur[4] + cur[5] + 12 + 7); i++)
 		cout << "-";
 	cout << endl;
 }
 
+// Заполнение пробелами для выравнивания таблицы
 void bl::printSpace(int max, int cur)
 {
 	for (cur; cur < max; cur++)
 		cout << " ";
+}
+
+// Редактируем БО
+void bl::editBO(string curBO)
+{
+	vector<vector<int>> BO;
+
+	int pos = 0;
+	string cur = "";
+	if (curBO == "bType")
+	{
+		BO = bType;
+		pos = 1;
+		cur = "тип";
+	}
+	else if (curBO == "bSize")
+	{
+		BO = bSize;
+		pos = 2;
+		cur = "размер";
+	}
+	else if (curBO == "rSpeed")
+	{
+		BO = bRSpeed;
+		pos = 3;
+		cur = "скорость чтения";
+	}
+	else if (curBO == "wSpeed")
+	{
+		BO = bWSpeed;
+		pos = 4;
+		cur = "скорость записи";
+	}
+	else if (curBO == "price")
+	{
+		BO = bPrice;
+		pos = 5;
+		cur = "цена";
+	}
+	else
+		return;
+
+	for (int i = 0; i < BO.size(); i++)
+	{
+		for (int j = i; j < BO[i].size(); j++)
+		{
+			if (i == j){
+				BO[i][j] = 0;
+				continue;
+			}
+
+			cout << "\033[32m" << "Какой(ая) " << cur << " лучше? (0(никая) или 1 или 2)" << "\033[0m" << endl;
+			cout << allFlash[i].name;
+			cout << " (" << "\033[33m" << retCurRow(pos, allFlash[i]) << "\033[0m" << ")";
+			cout << " или ";
+			cout << allFlash[j].name;
+			cout << " (" << "\033[33m" << retCurRow(pos, allFlash[j]) << "\033[0m" << ")";
+			cout << endl;
+
+			int ans = -1;
+			while (ans != 0 && ans != 1 && ans != 2)
+			{
+				int x = 0;
+				while(!(cin >> x))
+				{
+        			cin.clear();
+        			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        			cout << "Ввести требуется только 0, 1 или 2, пробуйте еще: ";
+    			}
+
+    			if (x == 0 || x == 1 || x == 2)
+    				ans = x;
+    			else 
+    				cout << "Ввести требуется только 0, 1 или 2, пробуйте еще: ";
+			}
+
+			switch (ans)
+			{
+				case 0:
+					BO[i][j] = 0;
+					BO[j][i] = 0;
+					break;
+				case 1:
+					BO[i][j] = 1;
+					BO[j][i] = 0;
+					break;
+				case 2:
+					BO[i][j] = 0;
+					BO[j][i] = 1;
+					break;
+			}
+		}
+	}
+
+	if (curBO == "bType")
+		bType = BO;
+	else if (curBO == "bSize")
+		bSize = BO;
+	else if (curBO == "rSpeed")
+		bRSpeed = BO;
+	else if (curBO == "wSpeed")
+		bWSpeed = BO;
+	else if (curBO == "price")
+		bPrice = BO;
+
+}
+
+// Возращает число из текущей строки
+string bl::retCurRow(int pos, flash cur)
+{
+	switch (pos)
+	{
+		case 1:
+			return cur.standart;
+			break;
+		case 2:
+			return to_string(cur.size);
+			break;
+		case 3:
+			return to_string(cur.rSpeed);
+			break;
+		case 4:
+			return to_string(cur.wSpeed);
+			break;
+		case 5:
+			return to_string(cur.	price);
+			break;
+		default:
+			return "";
+	}
 }
--- homeWork/cw/src/main.cpp
+++ homeWork/cw/src/main.cpp
@@ -1,11 +1,164 @@
 #include "main.h"
 
+struct menu
+{
+	string menu;
+	string BO;
+	int min;
+	int max;
+	bool close;
+
+};
+
+menu mMenu;
+// Первое какое меню 
+
+void viewAllTable(bl* main){
+	main->mPrintTable();
+
+	cout << endl;
+	main->bPrintTable("bType");
+
+	cout << endl;
+	main->bPrintTable("bSize");
+
+	cout << endl;
+	main->bPrintTable("rSpeed");
+
+	cout << endl;
+	main->bPrintTable("wSpeed");
+
+	cout << endl;
+	main->bPrintTable("price");
+
+
+}
+
+void viewBO(bl* main)
+{
+	main->bPrintTable(mMenu.BO);
+}
+
+int read()
+{
+	int ans = -1;
+	while (ans < mMenu.min || ans > mMenu.max)
+	{
+		int x = 0;
+		cout << "Выберите пункт: ";
+		while(!(cin >> x))
+		{
+    		cin.clear();
+    		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    		cout << "Не правильно, попробуйте еще. ";
+    	}
+
+    	if (x >= mMenu.min && x <= mMenu.max)
+    		ans = x;
+    	else 
+    		cout << "Не правильно, попробуйте еще. ";
+	}
+
+	return ans;
+}
+
+void menu(bl* main)
+{
+	cout << endl;
+
+	if (mMenu.menu == "main")
+	{
+		cout << "\033[33m" << "Главное меню:" << "\033[0m"<<endl;
+		cout << "1. Печать таблицы" << endl; 
+		cout << "2. Печать БО" << endl;
+		cout << "3. Редактровать БО" << endl;
+		cout << "4. Сохранить БО" << endl;
+		cout << "5. Печать всего" << endl;
+		cout << "0. Выход" << endl;
+	}
+
+	if (mMenu.menu == "vBO" || mMenu.menu == "eBO" || mMenu.menu == "wBO")
+	{
+		if (mMenu.menu == "eBO")
+			cout << "\033[33m" << "Редактировать БО:" << "\033[0m"<<endl;
+		else if (mMenu.menu == "wBO")
+			cout << "\033[33m" << "Сохранить БО:" << "\033[0m"<<endl;
+		else
+			cout << "\033[33m" << "Печать БО:" << "\033[0m"<<endl;
+		cout << "1. Версия USB" << endl; 
+		cout << "2. Размер памяти" << endl;
+		cout << "3. Скорость чтения" << endl;
+		cout << "4. Скорость записи" << endl;
+		cout << "5. Цена" << endl;
+		cout << "0. Выход" << endl;
+	}
+
+	int ans = read();
+
+	cout << endl;
+
+	if (mMenu.menu == "vBO")
+	{
+		if (ans == 1)
+			mMenu.BO = "bType";
+		if (ans == 2)
+			mMenu.BO = "bSize";
+		if (ans == 3)
+			mMenu.BO = "rSpeed";
+		if (ans == 4)
+			mMenu.BO = "wSpeed";
+		if (ans == 5)
+			mMenu.BO = "price";
+
+		if (ans != 0)
+			viewBO(main);
+
+		mMenu.menu = "main";
+		mMenu.min = 0;
+		mMenu.max = 5;
+	} 
+	else if (mMenu.menu == "main")
+	{
+		if (ans == 1) 
+			main->mPrintTable();
+		if (ans == 2) 
+		{
+			mMenu.menu = "vBO";
+			mMenu.min = 0;
+			mMenu.max = 5;
+		}
+		if (ans == 5)
+			viewAllTable(main);
+
+		if (ans == 0)
+			mMenu.close = true;
+	}
+	
+
+	
+}
+
+
 int main()
 {
-	bl* main = new bl("model.csv");
-	main->printTable();
+	bl* main = new bl("model.csv", "type.csv", "size.csv", "rSpeed.csv", "wSpeed.csv", "price.csv");
+	
+	mMenu.menu = "main";
+	mMenu.min = 0;
+	mMenu.max = 5;
+	mMenu.close = false;
+	while (!mMenu.close)
+		menu(main);
+
+	// cout << endl;
+	// main->editBO("bType");
+
+	// cout << endl;
+	// main->bPrintTable("bType");
 
 	return 0;
 }
+
+
 
 
